3231f66eb3ebdf865c2540cc98887e1331ea054e
Will Hardy <will@willhardy.com.au>
2018-09-29 00:28:13 +0200

    Cleanup, comments, optimisations

--- shortest_distance.py
+++ shortest_distance.py
@@ -1,30 +1,61 @@
 import pytest
 import string
+from typing import Iterable
 
 
-def tokenize_file(filename):
-    """ Tokenize the given file by words. """
+"""
+* To run the test suite, install pytest and run `pytest shortest_distance.py`.
+* To find the shortest distance on the provided file, call:
+    `shortest_distance(start, end)`
+* To find the distance on any given file, use:
+    `get_shortest_distance(lines, start, end)`
+"""
+
+
+def shortest_distance(start: str, end: str):
+    filename = 'test-input.txt'
     with open(filename, 'r') as f:
-        for line in f:
-            for word in line.split():
-                yield word.strip(string.punctuation).casefold()
+        return get_shortest_distance(f, start, end)
 
 
-def parse_file(filename, start, end):
-    """ Parse the given file and record the locations of the given relevant words. """
+def get_shortest_distance(lines: Iterable, start: str, end: str):
+    # Make our lookup case-insensitive
+    start, end = start.casefold(), end.casefold()
+
+    # We pass the words into the file parser, to allow it to optimise, if it so wishes
+    db = parse_content(lines, start, end)
+
+    try:
+        return min(get_candidate_distances(db[start], db[end]))
+
+    # An empty sequence will raise a ValueError, we choose to return None
+    except ValueError:
+        return None
+
+
+def tokenize(lines):
+    """ Tokenize the given file by words. """
+    for line in lines:
+        for word in line.split():
+            yield word.strip(string.punctuation).casefold()
+
+
+def parse_content(lines, start, end):
+    """ Parse the given file and return a data structure that we can use.
+
+    This data structure will be two lists of positions of start and end words.
+    """
 
     # Initialise our output data structure (a list of positions for each word)
     db = {start: [], end: []}
 
     # Add the position of any relevant words
-    for i, token in enumerate(tokenize_file(filename)):
+    for i, token in enumerate(tokenize(lines)):
+        seen_start_token = bool(db[start])
 
         # Always include the start position if listed
-        if token == start:
-            db[token].append(i)
-
-        # Only add end token positions if we have already seen a start position
-        elif db[start] and token == end:
+        # Only add end positions if we have already seen a start position
+        if token == start or (seen_start_token and token == end):
             db[token].append(i)
 
     return db
@@ -36,19 +67,16 @@ def distance(start, end):
     return end - start - 1
 
 
-def shortest_distance(start, end):
-    # Make our lookup case-insensitive
-    start, end = start.casefold(), end.casefold()
-
-    # We pass the words into the file parser, to allow it to optimise, if it so wishes
-    db = parse_file('test-input.txt', start, end)
-
-    try:
-        return min(distance(s, e) for e in db[end] for s in db[start] if s < e)
-
-    # An empty sequence will raise a ValueError, we choose to return None
-    except ValueError:
-        return None
+def get_candidate_distances(start_positions, end_positions):
+    # This only includes one optimisation, but it might be possible to include
+    # more, for example by iterting through the start positions in reverse
+    for start in start_positions:
+        for end in end_positions:
+            if start < end:
+                yield distance(start, end)
+                # There's no need to consider any other end positions, they will
+                # only be longer.
+                continue
 
 
 @pytest.mark.parametrize('start,end,exp_distance', [
@@ -76,5 +104,15 @@ def shortest_distance(start, end):
     ('motivation', 'motivation', None),
     ('development', 'development', 1),
 ])
-def test_shortest_distance(start, end, exp_distance):
+def test_with_provided_content(start, end, exp_distance):
     assert shortest_distance(start, end) == exp_distance
+
+
+@pytest.mark.parametrize('start,end,exp_distance', [
+    ('1', '1', 0),
+    ('2', '4', 1),
+    ('4', '3', 2),
+])
+def test_with_difficult_content(start, end, exp_distance):
+    content = ['  1 2 3 2 1 1 4 2 2 3 4 1 1 5 3 4 1 1 3']
+    assert get_shortest_distance(content, start, end) == exp_distance
